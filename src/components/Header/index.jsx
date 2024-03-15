e002f6a33dff68f799e091e1bda12758ddf6f0a9
AdamSkubida <adasiow.s@gmail.com>
2024-03-15 12:35:57 +0100

    update: Header styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,9 @@ import css from "./Header.module.css";
 export const Header = () => {
   return (
     <>
-      <div>SiWear</div>
+      <div className={css.logo}>
+        <a href="https://adamskubida.github.io/fasion-page/">SiWear</a>
+      </div>
     </>
   );
 };
