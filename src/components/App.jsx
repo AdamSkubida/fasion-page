818484c6aa595bddc1e0e2d22c8e922f5abacac1
AdamSkubida <adasiow.s@gmail.com>
2024-03-15 13:10:53 +0100

    update: nav added to Header + styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import { Header } from "./Header/index.jsx";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,20 @@ import css from "./Header.module.css";
 export const Header = () => {
   return (
     <>
-      <div className={css.logo}>
-        <a href="https://adamskubida.github.io/fasion-page/">SiWear</a>
+      <div className={css.container}>
+        <div className={css.logo}>
+          <a href="https://adamskubida.github.io/fasion-page/">SiWear</a>
+        </div>
+        <nav>
+          <ul className={css.list}>
+            <li className={css["list-item"]}>
+              <a href="https://adamskubida.github.io/fasion-page/">Home</a>
+            </li>
+            <li className={css["list-item"]}>
+              <a href="">Me</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
