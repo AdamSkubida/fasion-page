d222d0177ed43773d1407e2435971e1c638c0fb2
AdamSkubida <adasiow.s@gmail.com>
2024-03-16 19:40:56 +0100

    update: Header @media styles, background color

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
         <div className={css.logo}>
           <a href="https://adamskubida.github.io/fasion-page/">SiWear</a>
         </div>
-        <nav>
+        <nav className={css.nav}>
           <ul className={css.list}>
             <li className={css["list-item"]}>
               <a href="https://adamskubida.github.io/fasion-page/">Home</a>
