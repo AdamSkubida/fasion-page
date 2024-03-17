5737b2090f4a58682f2acc71bdc9e0c4670f327a
AdamSkubida <adasiow.s@gmail.com>
2024-03-17 23:20:13 +0100

    update: Modal -> close button styles

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,11 +1,12 @@
 import css from "./Modal.module.css";
+import { IoMdClose } from "react-icons/io";
 
 export const Modal = ({ onClose }) => {
   return (
     <div className={css.overlay}>
       <div className={css.modal}>
-        <button className={css["modal-header"]} onClick={() => onClose()}>
-          &times;
+        <button className={css["close-button"]} onClick={() => onClose()}>
+          <IoMdClose className={css.close} />
         </button>
         <div className={css["modal-content"]}>
           <ul className={css.list}>
