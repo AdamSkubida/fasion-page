7b05938c81d57b36ca7d3538c7c0f2e9324fdbfd
AdamSkubida <adasiow.s@gmail.com>
2024-03-18 16:56:19 +0100

    add: Main -> gallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Header } from "./Header/index.jsx";
 import { Modal } from "./Modal/index.jsx";
+import { Main } from "./Main/index.jsx";
 
 const App = () => {
   const [modalOpen, setModalOpen] = useState(true);
@@ -17,6 +18,7 @@ const App = () => {
     <>
       <Header onClick={openModalHandler} />
       {modalOpen && <Modal onClose={closeModalHandle} />}
+      <Main />
     </>
   );
 };
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,40 @@
+import css from "./Main.module.css";
+import img1 from "/elegant-shoes640.jpg";
+import img2 from "/shoes640.jpg";
+import img3 from "/nike640.jpg";
+import img4 from "/jumper640.jpg";
+import img5 from "/dress640.jpg";
+import img6 from "/jeans640.jpg";
+
+export const Main = () => {
+  return (
+    <>
+      <div className={css.gallery}>
+        <div className={css["img-wrapper"]}>
+          <img src={img1} alt="elegant shoes" />
+          <p className={css.price}>300$</p>
+        </div>
+        <div className={css["img-wrapper"]}>
+          <img src={img2} alt="shoes" />
+          <p className={css.price}>200$</p>
+        </div>
+        <div className={css["img-wrapper"]}>
+          <img src={img5} alt="dress" />
+          <p className={css.price}>180$</p>
+        </div>
+        <div className={css["img-wrapper"]}>
+          <img src={img3} alt="nike" />
+          <p className={css.price}>250$</p>
+        </div>
+        <div className={css["img-wrapper"]}>
+          <img src={img6} alt="jeans" />
+          <p className={css.price}>120$</p>
+        </div>
+        <div className={css["img-wrapper"]}>
+          <img src={img4} alt="jumper" />
+          <p className={css.price}>120$</p>
+        </div>
+      </div>
+    </>
+  );
+};
