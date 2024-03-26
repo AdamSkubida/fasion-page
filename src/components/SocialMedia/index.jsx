6bf3bbec11e77dfe2c39842f9ea3ae7b8539488b
AdamSkubida <adasiow.s@gmail.com>
2024-03-26 12:46:42 +0100

    update: SocialMedia -> backround-color, transition, icon color, margin

--- src/components/SocialMedia/index.jsx
+++ src/components/SocialMedia/index.jsx
@@ -1,19 +1,34 @@
 import css from "./SocialMedia.module.css";
+import { useState } from "react";
 import { SlSocialInstagram } from "react-icons/sl";
 import { SlSocialFacebook } from "react-icons/sl";
 import { MdMailOutline } from "react-icons/md";
 
 export const SocialMedia = () => {
+  const [isHovered, setIsHovered] = useState(null);
+
   return (
     <div className={css.wrapper}>
       <div className={css.social}>
-        <div className={css["icon-wrapper"]}>
+        <div
+          className={`${css.box} ${isHovered === 1 && css.ig}`}
+          onMouseEnter={() => setIsHovered(1)}
+          onMouseLeave={() => setIsHovered(null)}
+        >
           <SlSocialInstagram className={css.icon} />
         </div>
-        <div className={css["icon-wrapper"]}>
+        <div
+          className={`${css.box} ${isHovered === 2 && css.fb}`}
+          onMouseEnter={() => setIsHovered(2)}
+          onMouseLeave={() => setIsHovered(null)}
+        >
           <SlSocialFacebook className={css.icon} />
         </div>
-        <div className={css["icon-wrapper"]}>
+        <div
+          className={`${css.box} ${isHovered === 3 && css.mail}`}
+          onMouseEnter={() => setIsHovered(3)}
+          onMouseLeave={() => setIsHovered(null)}
+        >
           <MdMailOutline className={css.icon} />
         </div>
       </div>
