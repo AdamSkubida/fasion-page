462434660b1f8f2f3d6f7f95016d69cf82043723
AdamSkubida <adasiow.s@gmail.com>
2024-04-25 12:50:21 +0200

    update/Advert -> styles

--- src/components/Advert/index.jsx
+++ src/components/Advert/index.jsx
@@ -11,8 +11,22 @@ export const Advert = ({ onClose }) => {
         </button>
         <div className={css["modal-content"]}>
           <div className={css.info}>
-            Hi! If something caught your eye , please contact me via my
-            Instagram, Facebook, email, or phone number
+            <span className={css.text}>
+              <b>Hi!</b> If something <b>caught</b> your eye , please{" "}
+              <b>contact</b> me via my:
+            </span>
+            <Link to="https://www.instagram.com/">
+              <p className={css.ig}>Instagram</p>
+            </Link>
+            <Link to="https://www.facebook.com/">
+              <p className={css.fb}>Facebook</p>
+            </Link>
+            <Link to="mailto:siwear@gmail.com">
+              <p className={css.email}>email</p>
+            </Link>
+            <Link to="tel:123-456-789">
+              <p className={css.phone}>phone number</p>
+            </Link>
           </div>
         </div>
       </div>
