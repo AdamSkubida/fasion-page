9319ecf863f3d69592451a83b9536b0c4be9c2af
AdamSkubida <adasiow.s@gmail.com>
2024-11-23 03:42:38 +0100

    Header update/changed layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,22 +6,35 @@ export const Header = ({ onClick }) => {
   return (
     <>
       <div className={css.container}>
-        <div className={css.logo}>
-          <Link to="/">SiWear</Link>
+        <div className={css.wrapper}>
+          <nav className={css.nav}>
+            <ul className={css.list}>
+              <li className={css["list-item"]}>
+                <Link to="/">Home</Link>
+              </li>
+              <li className={css["list-item"]}>
+                <Link to="/Me">Me</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={css.logo}>
+            <Link to="/">Pogadajmy</Link>
+          </div>
+          <div className={css.contact}>
+            <ul className={css["contact-list"]}>
+              <li className={css["contact-item"]}>
+                <a href="tel:+48123456789">+48 123 456 789</a>
+              </li>
+              <li className={css["contact-item"]}>
+                <a href="mailto:pogadajmy@gmail.com">pogadajmy@gmail.com</a>
+              </li>
+            </ul>
+          </div>
+
+          <button className={css["open-button"]} onClick={() => onClick()}>
+            <RxHamburgerMenu className={css.burger} />
+          </button>
         </div>
-        <nav className={css.nav}>
-          <ul className={css.list}>
-            <li className={css["list-item"]}>
-              <Link to="/">Home</Link>
-            </li>
-            <li className={css["list-item"]}>
-              <Link to="/Me">Me</Link>
-            </li>
-          </ul>
-        </nav>
-        <button className={css["open-button"]} onClick={() => onClick()}>
-          <RxHamburgerMenu className={css.burger} />
-        </button>
       </div>
     </>
   );
